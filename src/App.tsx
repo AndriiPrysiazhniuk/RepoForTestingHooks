b66ed67364167331c536306e8d0d458a792606fb
AndriiPrysiazhniuk <sarutora81@gmail.com>
2023-03-02 19:31:25 +0200

    optimized app. deleted unused constant

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {Button} from "./Button";
 type FitlerType = 'all' | 'dollars' | 'rubles'
 export const App = () => {
 
-        const [money, setMoney] = useState([
+        const money=[
             {banknots: 'dollars', value: 100, number: ' a1234567890'},
             {banknots: 'dollars', value: 50, number: ' z1234567890'},
             {banknots: 'rubls', value: 100, number: ' w1234567890'},
@@ -16,7 +16,7 @@ export const App = () => {
             {banknots: 'rubls', value: 100, number: ' r1234567890'},
             {banknots: 'dollars', value: 50, number: ' x1234567890'},
             {banknots: 'rubls', value: 50, number: ' v1234567890'},
-        ])
+        ]
         const [filter, setFilter] = useState<FitlerType>('all')
 
     let currentMoney = money
