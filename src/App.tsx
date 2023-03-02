d7e1f9b74e6bbc86b828d90ef4a451abcf6e7347
AndriiPrysiazhniuk <sarutora81@gmail.com>
2023-03-02 19:21:29 +0200

    ended filter of banknots

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,85 @@ import React, {useState} from 'react';
 import './App.css'
 import {Button} from "./Button";
 
+
+
+type FitlerType = 'all' | 'dollars' | 'rubles'
 export const App = () => {
-// let num =1
-    const [num, setNum] = useState(1)
-    const increaseNum = () => {
-        if (num >= 0 && num<10) {
-            // num <= 10
-            setNum(num + 1)
+
+        const [money, setMoney] = useState([
+            {banknots: 'dollars', value: 100, number: ' a1234567890'},
+            {banknots: 'dollars', value: 50, number: ' z1234567890'},
+            {banknots: 'rubls', value: 100, number: ' w1234567890'},
+            {banknots: 'dollars', value: 100, number: ' e1234567890'},
+            {banknots: 'dollars', value: 50, number: ' c1234567890'},
+            {banknots: 'rubls', value: 100, number: ' r1234567890'},
+            {banknots: 'dollars', value: 50, number: ' x1234567890'},
+            {banknots: 'rubls', value: 50, number: ' v1234567890'},
+        ])
+        const [filter, setFilter] = useState<FitlerType>('all')
+
+    let currentMoney = money
+        if (filter === 'dollars') {
+            currentMoney = money.filter(el => el.banknots === 'dollars')
         }
-        if(num ===10 ){
-            setNum(0)
+        if (filter === 'rubles') {
+             currentMoney = money.filter(el => el.banknots === 'rubls')
         }
+        const onClickFilterHandler = (NameButton:FitlerType) => {
+            // setMoney(title)
+            setFilter(NameButton)
+        }
+
+
+        return (
+            <div className={'App'}>
+                {currentMoney.map(el => {
+                    return (
+                        <div key={el.number}>
+                            <span>{el.banknots} - </span>
+                            <span>{el.value} - </span>
+                            <span>{el.number}</span>
+                        </div>
+                    )
+                })}
+                <Button title={'all'} callback={() => onClickFilterHandler('all')}/>
+                <Button title={'ruble'} callback={() => onClickFilterHandler('rubles')}/>
+                <Button title={'dollars'} callback={() => onClickFilterHandler('dollars')}/>
+
+                {/*<h1>{num}</h1>*/}
+                {/*<Button title={'reset'} callback={resetNum}/>*/}
+            </div>
+        );
     }
-    const resetNum=()=>{
-        setNum(0)
-    }
-    return (
-        <div className={'App'}>
-            <h1>{num}</h1>
-            <Button title={'click me to +'} callback={increaseNum}/>
-            <Button title={'reset'} callback={resetNum}/>
-        </div>
-    );
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  let num =1
+//     const [num, setNum] = useState(1)
+//     const increaseNum = () => {
+//         if (num >= 0 && num<10) {
+//             // num <= 10
+//             setNum(num + 1)
+//         }
+//         if(num ===10 ){
+//             setNum(0)
+//         }
+//     }
+//     const resetNum=()=>{
+//         setNum(0)
+//     }
