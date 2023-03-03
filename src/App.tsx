d94e1834d6103b7aa9ec38bb34c2f43da9060066
AndriiPrysiazhniuk <sarutora81@gmail.com>
2023-03-03 10:14:18 +0200

    added sorting array by alphabet

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,9 @@ export const App = () => {
     const [num, setNum] = useState(1)
     const increaseNum = () => {
         if (num >= 0 && num < 10) {
-            // num <= 10
             setNum(num + 1)
         }
         if (num === 10) {
-
             setNum(0)
         }
     }
@@ -25,14 +23,25 @@ export const App = () => {
 
     const money = [
         {banknots: 'dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'rubls', value: 100, number: ' w1234567890'},
         {banknots: 'dollars', value: 100, number: ' e1234567890'},
         {banknots: 'dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'dollars', value: 50, number: ' f1234567890'},
+        {banknots: 'dollars', value: 50, number: ' d1234567890'},
+        {banknots: 'rubls', value: 100, number: ' b1234567890'},
         {banknots: 'rubls', value: 100, number: ' r1234567890'},
-        {banknots: 'dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'rubls', value: 50, number: ' v1234567890'},
-    ]
+        {banknots: 'rubls', value: 50, number: ' g1234567890'},
+    ];
+    money.sort(function sortByAlphabet(a, b) {
+
+        if (a.number < b.number) {
+            return -1;
+        }
+        if (a.number > b.number) {
+            return 1;
+        }
+        return 0;
+    });
+
 
     const [filter, setFilter] = useState<FilterType>('all')
 
@@ -41,15 +50,16 @@ export const App = () => {
         currentMoney = money.filter(el => el.banknots === 'rubls')
     }
     if (filter === 'dollars') {
-        currentMoney = money.filter(el => el.banknots ==='dollars')
+        currentMoney = money.filter(el => el.banknots === 'dollars')
     }
     const onClickHandler = (message: FilterType) => {
         setFilter(message)
-    }
+        // git -am 'added sorting array by alphabet'
 
+    }
     return (
         <div className={'App'}>
-            <h1>{filter}</h1>
+            <h1>{filter}</h1> <Button title={'sort by alphabet'} callback={()=>{}}/>
             {currentMoney.map(el => {
                 return (
                     <div key={el.number}>
